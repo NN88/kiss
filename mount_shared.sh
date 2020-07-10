c46bc5ffa2f4dca663d1646bd8dac6fe2cad5f60
NN88 <noneofyourdamnbusinessbuttout@something.com>
2020-07-10 17:52:42 -0400

    Update mount_shared.sh

--- mount_shared.sh
+++ mount_shared.sh
@@ -12,17 +12,11 @@ echo " Mount Up!!!!!!! "
 echo " mount 'shared' folder which is hosted on master OS to VMWare Kali Linux Guest OS"
 echo " Make sure you are fully updated, and upgraded. Run the update.sh script if not "
 echo "________________________________________________ \n"
-echo "\n===== Make ~/mnt/hgfs mount point on Guest OS  ====="
-mkdir -p /mnt/hgfs
 
 echo "\n===== Mount Shared Folder from Master OS to Guest OS  ====="
-vmware-hgfsclient | while read folder; do
-  vmwpath="/mnt/hgfs/\${folder}"
-  echo "[i] Mounting \${folder}   (\${vmwpath})"
-  sudo mkdir -p "\${vmwpath}"
-  sudo umount -f "\${vmwpath}" 2>/dev/null
-  sudo vmhgfs-fuse -o allow_other -o auto_unmount ".host:/\${folder}" "\${vmwpath}"
-done
-sleep 2s
+sudo mount -t fuse.vmhgfs-fuse .host:/ /mnt/hgfs -o allow_other
+
+echo "\n===== Symlink Shared Folder to Desktop  ====="
+ln -s /mnt/hgfs/shared ~/Desktop/shared
 
 echo "\n\n ======== Happy Hacking ========== \n\n"
