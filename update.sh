1e4373c1997231943c1215a257af914fce7b85eb
NN88 <fuckoff@bitemeandfuckoff.com>
2021-01-25 20:02:15 -0500

    Remove -y

--- update.sh
+++ update.sh
@@ -11,16 +11,16 @@ echo " Update Kali "
 echo "______________________________________\n"
 echo "set -e is active \n"
 echo "===== apt-get TO STABLE ENVIRONMENT ===== \n"
-echo "\n * apt-get update -y *"
+echo "\n * apt-get update *"
 echo "______________________________________"
-apt-get update -y
+apt-get update
 echo "\n * apt-get upgrade -y *"
 echo "______________________________________"
-apt-get upgrade -y
-echo "\n * apt-get dist-upgrade -y  *"
+apt-get upgrade
+echo "\n * apt-get dist-upgrade  *"
 echo "______________________________________"
-apt-get dist-upgrade -y
-echo "\n * apt-get autoclean -y *"
+apt-get dist-upgrade
+echo "\n * apt-get autoclean *"
 echo "______________________________________"
-apt-get autoclean -y
+apt-get autoclean
 echo "\n\n===== Happy Hacking! ===== \n\n"
